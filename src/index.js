0ca749a16c252c39b4b5ccafe8c83302f104d19f
Jenny Belanger <jbelangr@gmail.com>
2022-12-27 10:37:08 -0500

    Fix deprecation warning

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import './scss/style.scss';
 import { App } from '@jlbelanger/crudnick';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import Routes from './js/Routes';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 	<React.StrictMode>
 		<App
 			nav={[
@@ -20,6 +21,5 @@ ReactDOM.render(
 		>
 			<Routes />
 		</App>
-	</React.StrictMode>,
-	document.getElementById('root')
+	</React.StrictMode>
 );
